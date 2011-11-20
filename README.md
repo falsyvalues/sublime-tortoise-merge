cc544db457fc5342fe1feb91f6cf127f10ec7ead
Michał Lipiński <mylith@gmail.com>
2011-11-20 16:52:28 +0100

    less is more

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Description
 ===========
-[TortoiseMerge](http://tortoisesvn.tigris.org/TortoiseMerge.html) is a simple tool commonly used in Tortoise products [TortoiseSVN](http://tortoisesvn.net/) and [TortoiseGit](http://code.google.com/p/tortoisegit/). This Sublime Text 2 package adds feature to diff files using TortoiseMerge, available from side bar menu.
+[TortoiseMerge](http://tortoisesvn.tigris.org/TortoiseMerge.html) is a simple tool commonly used in Tortoise products [TortoiseSVN](http://tortoisesvn.net/) and [TortoiseGit](http://code.google.com/p/tortoisegit/). This Sublime Text 2 package adds feature to diff files using TortoiseMerge, available from side bar menu and last two active files via key shortcut (see sublime-keymap).
--- TortoiseMerge.py
+++ TortoiseMerge.py
@@ -1,19 +1,40 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 import os
-import subprocess
+from subprocess import Popen
 
 PATHS = ['TortoiseSVN', 'TortoiseGit']
 EXEC = 'bin\\TortoiseMerge.exe'
 
-class TortoiseMergeCommand(sublime_plugin.WindowCommand):
-    def __init__(self, view):
-        self._ = [];
+class TortoiseMerge:
+    filesToDiff = []
+    _ = []
+
+    def __init__(self):
         for item in PATHS:
             self._.append(os.environ['ProgramFiles'] + os.sep + item + os.sep + EXEC)
             self._.append(os.environ['ProgramFiles'] + ' (x86)' + os.sep + item + os.sep + EXEC)
 
-    def run(self, files):
-        source = None;
+    def addFile(self, name):
+        self.filesToDiff.append(name)
+
+        if len(self.filesToDiff) > 2:
+            self.filesToDiff = self.filesToDiff[1:3:]
+
+        return self
+
+    def isRunable(self):
+        if len(self.filesToDiff) != 2:
+            return False
+
+        if self.filesToDiff[0] == self.filesToDiff[1]:
+            return False
+
+        return True
+
+    def diff(self):
+        source = None
+
         for path in self._:        
             if os.path.exists(path):
                 source = path
@@ -21,12 +42,29 @@ class TortoiseMergeCommand(sublime_plugin.WindowCommand):
       
         if not source:
             print "TortoiseMerge not found."
-            return
+            return None
+        
+        if not self.isRunable():
+            print "Sorry pal, not enough files to run diff."
+            return None
       
-        cmd = '%s "%s" "%s"' % (source, files[0], files[1])
-        print "TortoiseMerge command: " + cmd
-        subprocess.Popen(cmd)
-   
-    # We have 2 files selected
+        cmd = '%s "%s" "%s"' % (source, self.filesToDiff[0], self.filesToDiff[1])
+        print "TortoiseMerge command: %s" % cmd
+        Popen(cmd)
+
+tm = TortoiseMerge()
+
+class TortoiseMergeFileListener(sublime_plugin.EventListener):
+    
+    def on_activated(self, view):
+        tm.addFile(view.file_name())
+
+class TortoiseMergeCommand(sublime_plugin.ApplicationCommand):
+
+    def run(self, files = []):
+        if len(files) > 1:
+            tm.addFile(files[1]).addFile(files[0])
+        tm.diff()
+
     def is_visible(self, files):
         return len(files) > 1
